3b0cda7a39cee20d1040f8ee4bf8210ab8870f15
shaheen5 <miyashaheen1995@gmail.com>
2021-04-27 15:38:10 +0530

    Added:LinkedList Of MyMapNode Having Key,Value Pair To Find Freq Of Words

--- main/java/com/datastructures/MyHashMap.java
+++ main/java/com/datastructures/MyHashMap.java
@@ -1,7 +1,28 @@
 package com.datastructures;
 
-public class MyHashMap {
-    public static void main(String[] args) {
-        System.out.println("Data Structures-Hash Table And Trees Problem");
+public class MyHashMap<K,V> {
+    MyLinkedList<K> myLinkedList ;
+
+    public MyHashMap(){
+        this.myLinkedList=new MyLinkedList<>();
+    }
+    //method to get value of node having given key
+    public V get(K key) {
+        MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) this.myLinkedList.search(key);
+        return (myMapNode == null) ? null : myMapNode.getValue();
+    }
+    //method to add key and value to hashmap node
+    public void add(K key, V value) {
+        MyMapNode<K,V> myMapNode= (MyMapNode<K, V>) myLinkedList.search(key);
+        if(myMapNode == null) {
+            myMapNode=new MyMapNode<>(key,value);
+            this.myLinkedList.append(myMapNode);
+        }else{
+            myMapNode.setValue(value);
+        }
+    }
+    @Override
+    public String toString() {
+        return "MyHashMapNodes{"+myLinkedList+'}';
     }
 }
---
+++ main/java/com/datastructures/MyLinkedList.java
@@ -0,0 +1,68 @@
+package com.datastructures;
+
+public class MyLinkedList <K>{
+    public INode head;
+    public INode tail;
+
+    public void MyLinkedList(){
+        this.head=null;
+        this.tail=null;
+    }
+    //method to add new node to linked list
+    public void add(INode newNode) {
+        if(this.tail == null) {
+            this.tail=newNode;
+        }
+        if(this.head == null) {
+            this.head=newNode;
+        }
+        else{
+            INode tempNode = this.head;
+            this.head=newNode;
+            this.head.setNext(tempNode);
+        }
+    }
+    // method to display node data
+    public void printMyNodes() {
+        StringBuffer myNodes = new StringBuffer("My Nodes : ");
+        INode tempNode = head;
+        while(tempNode.getNext() != null){
+            myNodes.append(tempNode.getKey());
+            if(!tempNode.equals(tail))
+                myNodes.append("->");
+            tempNode = tempNode.getNext();
+        }
+        myNodes.append(tempNode.getKey());
+        System.out.println(myNodes);
+    }
+
+    //method to append node at end
+    public void append(INode newNode) {
+        if(this.head == null) {
+            this.head=newNode;
+        }
+        if(this.tail == null) {
+            this.tail=newNode;
+        }
+        else{
+            this.tail.setNext(newNode);
+            this.tail = newNode ;
+        }
+    }
+
+    //method to search node by given key
+    public INode search(K key) {
+        INode tempNode=head;
+        INode searchedNode = null;
+        while(tempNode != null && tempNode.getNext() != null){
+            if(tempNode.getKey().equals(key))
+                searchedNode=tempNode;
+            tempNode=tempNode.getNext();
+        }
+        return searchedNode;
+    }
+    @Override
+    public String toString() {
+        return " MyLinkedListNodes{ "+ head + "}";
+    }
+}
