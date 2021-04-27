d65dd8dc3018a11bc586c57088a7116a822dfd56
shaheen5 <miyashaheen1995@gmail.com>
2021-04-27 07:07:49 +0530

    Added : Code To Display Welcome Message

---
+++ main/java/com/datastructures/MyHashMap.java
@@ -0,0 +1,7 @@
+package com.datastructures;
+
+public class MyHashMap {
+    public static void main(String[] args) {
+        System.out.println("Data Structures-Hash Table And Trees Problem");
+    }
+}
