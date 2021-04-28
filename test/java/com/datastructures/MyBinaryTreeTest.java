95a8c72436a99efea2142db11dd38ebf63a48f3c
shaheen5 <miyashaheen1995@gmail.com>
2021-04-28 20:05:46 +0530

    Added : Code To Create & Test Binary Search Tree

---
+++ test/java/com/datastructures/MyBinaryTreeTest.java
@@ -0,0 +1,16 @@
+package com.datastructures;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyBinaryTreeTest {
+    @Test
+    public void given3Numbers_WhenAddedToBinaryTree_ShouldReturnSizeThree(){
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        int size = myBinaryTree.getSize();
+        Assert.assertEquals(3,size);
+    }
+}
