98429ae760e1b3c32d98a405b1ae64eca62c877b
shaheen5 <miyashaheen1995@gmail.com>
2021-05-05 10:58:29 +0530

    Added : Code To Add Given Binary Tree Nodes & Test Size

--- main/java/com/datastructures/MyBinaryTree.java
+++ main/java/com/datastructures/MyBinaryTree.java
@@ -2,11 +2,11 @@ package com.datastructures;
 
 public class MyBinaryTree<K extends Comparable<K>> {
     private MyBinaryNode<K> root;
-
+    //add binary node to tree having given key value
     public void add(K key) {
         this.root = this.addRecursively(root,key);
     }
-
+    //add recursively by traversing left & right
     private MyBinaryNode<K> addRecursively(MyBinaryNode<K> current, K key) {
         if(current == null)
             return new MyBinaryNode<>(key);
@@ -20,13 +20,26 @@ public class MyBinaryTree<K extends Comparable<K>> {
         }
         return current;
     }
-
+    //get total nodes in binary tree
     public int getSize() {
         return this.getSizeRecursive(root);
     }
-
+    //find size by recursively traversing left & right nodes
     private int getSizeRecursive(MyBinaryNode<K> current) {
         return current == null ? 0 : 1 + this.getSizeRecursive(current.left) +
                                          this.getSizeRecursive(current.right);
+    }
+    //display binary tree nodes
+    public void displayBinaryTree() {
+        preOrderTraversal(this.root);
+    }
+    //traverse binary tree as pre order traversal
+    public void preOrderTraversal(MyBinaryNode root) {
+        if(root !=  null) {
+            //Visit the node by Printing the node data
+            System.out.printf("%d ",root.key);
+            preOrderTraversal(root.left);
+            preOrderTraversal(root.right);
+        }
     }
 }
--- test/java/com/datastructures/MyBinaryTreeTest.java
+++ test/java/com/datastructures/MyBinaryTreeTest.java
@@ -6,11 +6,31 @@ import org.junit.Test;
 public class MyBinaryTreeTest {
     @Test
     public void given3Numbers_WhenAddedToBinaryTree_ShouldReturnSizeThree(){
-        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<Integer>();
         myBinaryTree.add(56);
         myBinaryTree.add(30);
         myBinaryTree.add(70);
         int size = myBinaryTree.getSize();
         Assert.assertEquals(3,size);
+    }
+    @Test
+    public void givenNumbers_WhenAddedToBinaryTree_ShouldReturnCorrectSize(){
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<Integer>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        myBinaryTree.add(22);
+        myBinaryTree.add(40);
+        myBinaryTree.add(11);
+        myBinaryTree.add(60);
+        myBinaryTree.add(63);
+        myBinaryTree.add(3);
+        myBinaryTree.add(16);
+        myBinaryTree.add(67);
+        myBinaryTree.add(95);
+        myBinaryTree.add(65);
+        int size = myBinaryTree.getSize();
+        myBinaryTree.displayBinaryTree();
+        Assert.assertEquals(13,size);
     }
 }
